6ea7fd507c01daee64811c54038c5aa79eddb815
gittrine <trine.skadal@bouvet.no>
2017-01-27 23:19:14 +0100

    Trine!!

--- Controllers/BestillingController.cs
+++ Controllers/BestillingController.cs
@@ -11,37 +11,13 @@ namespace TitanicWebService.Controllers
 {
     public class BestillingController : ApiController
     {
-        public PersonligSannsynlighet[] Put([FromBody]Bestilling bestilling)
+        public decimal Get(string klasse, string kjoenn, int alder, string avreisesteg)
         {
             try
             {
-                PersonligSannsynlighet[] sann = new PersonligSannsynlighet[bestilling.Passasjerer.Count()];
-                DataTable dt;
-                int i = 0;
-                foreach(Passasjer pass in bestilling.Passasjerer)
-                {
-                     SqlParameter[] par = new SqlParameter[6];
-                    par[0] = new SqlParameter("Klasse", SQLUtil.HentNullableParameter(pass.Klasse));
-                    par[1] = new SqlParameter("Kjoenn", SQLUtil.HentNullableParameter(pass.Kjoenn));
-                    par[2] = new SqlParameter("Alder", SQLUtil.HentNullableParameter(pass.Alder));
-                    par[3] = new SqlParameter("Avreisested", SQLUtil.HentNullableParameter(bestilling.Avreisested));
-                    par[4] = new SqlParameter("AntallReisende", SQLUtil.HentNullableParameter(bestilling.AntallReisende));
-                    par[5] = new SqlParameter("AntallBarn", SQLUtil.HentNullableParameter(bestilling.AntallBarn));
-
-                    //dt = SQLUtil.ExecuteStoredProcedure("dbo.HentSannsynlighet", par);
-                    //if (dt.Rows.Count() = 0)
-                    //{
-                    //    throw new Exception("Ingen rader funnet");
-                    //}
-                    //DataRow dr = dt.Rows[0];
                     MLWrapper wrap = new MLWrapper()
-                    var sannsynlighet = wrap.GetTelthy(pass.Klasse, pass.Kjoenn, Int.Parse(pass.Alder), pass.Avreisested).Lehtability.ToString("f2");
-                    PersonligSannsynlighet sannp = new PersonligSannsynlighet();
-
-                    sannp.PassasjerID = pass.PassasjerID;
-                    sannp.Sannsynlighet = sannsynlighet;//(decimal)dr["Sannsynlighet"];
-
-                    sann[i] = sannp;
+                    var sannsynlighet = wrap.GetTelthy(klasse, kjoenn, alder, avreisested).Lehtability.ToString("f2");
+                    return sannsynlighet
                 }
                 return sann;
             }
@@ -56,32 +32,34 @@ namespace TitanicWebService.Controllers
             }
         }
 
-         public Bestilling Get()
-        {
-            try
-            {
-                Bestilling best = new Bestilling();
-                best.Passasjerer = new Passasjer[1];
+        // public Bestilling Get()
+        //{
+        //    try
+        //    {
+        //        Bestilling best = new Bestilling();
+        //        best.Passasjerer = new Passasjer[1];
 
-                Passasjer pass = new Passasjer();
-                pass.PassasjerID = 1;
-                pass.Alder = 34;
-                pass.Kjoenn = "Kvinne";
-                pass.Klasse = "Klasse1";
-
-                best.Passasjerer[0] = pass;
-                return best;
-            }
-            catch (Exception e)
-            {
-                var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError)
-                {
-                    Content = new StringContent(e.Message),
-                    ReasonPhrase = "Server error."
-                };
-                throw new HttpResponseException(resp);
-            }
-         }
+        //        Passasjer pass = new Passasjer();
+        //        pass.PassasjerID = 1;
+        //        pass.Alder = 34;
+        //        pass.Kjoenn = "Kvinne";
+        //        pass.Klasse = "Klasse1";
+        //          MLWrapper wrap = new MLWrapper()
+        //            var sannsynlighet = wrap.GetTelthy(pass.Klasse, pass.Kjoenn, Int.Parse(pass.Alder), pass.Avreisested).Lehtability.ToString("f2");
+        //        pass.s
+        //        best.Passasjerer[0] = pass;
+        //        return best;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+        //        {
+        //            Content = new StringContent(e.Message),
+        //            ReasonPhrase = "Server error."
+        //        };
+        //        throw new HttpResponseException(resp);
+        //    }
+        // }
 
     }
 }
